9d4cf4ec17bba94484e0b5beeccc62cdc8ca29f3
iloncka-ds <iloncka.ds@gmail.com>
2022-09-28 23:42:15 +0300

    First experiment

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Service for car license plate number, type, color, moving direction automatic detection',
+    author='DL-group-N3',
+    license='MIT',
+)
