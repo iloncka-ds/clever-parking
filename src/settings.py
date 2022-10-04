3b040d21102cde9c602c9cee3e52ee2c4a3a9bce
pacificus <masterkristall@gmail.com>
2022-10-04 21:03:48 +0700

    remove unused files

--- src/settings.py
+++
@@ -1,9 +0,0 @@
-import os
-
-import dotenv
-
-project_dir = os.path.join(os.path.dirname(__file__), os.pardir)
-dotenv_path = os.path.join(project_dir, ".env")
-dotenv.load_dotenv(dotenv_path)
-VIDEO_PATH = os.environ.get("VIDEO_PATH")
-OUTPUT_PATH = os.environ.get("OUTPUT_PATH")
